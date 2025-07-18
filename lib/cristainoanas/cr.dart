08f0a5d430e105f0c72ddfbe9ef6bbac53b1aad4
rashiqmuhammed273 <141703297+rashiqmuhammed273@users.noreply.github.com>
2025-07-18 16:24:34 +0530

    created a folder in lib

---
+++ lib/cristainoanas/cr.dart
@@ -0,0 +1,44 @@
+
+// import 'package:flutter/material.dart';
+
+
+
+// class Imagealternate extends StatelessWidget {
+// const Imagealternate({super.key});
+
+// @override
+//   Widget build(BuildContext context) {
+//      return Container(color: Colors.blue,
+     
+      
+//        child: Image.asset(
+//          'assets/userlogo.png',
+//          width: 85,        // resize here
+//          height: 85,
+        
+//        ),
+//      );
+//   }}
+
+
+
+  
+// class Agencyprofileimage extends StatelessWidget {
+// const Agencyprofileimage({super.key});
+
+// @override
+//   Widget build(BuildContext context) {
+//      return CircleAvatar(
+//         radius: 42.5,
+//        child: Container(color: Colors.blue,
+       
+        
+//          child: Image.asset(
+//            'assets/companieslogo.png',
+//            width: 85,        // resize here
+//            height: 85,
+          
+//          ),
+//        ),
+//      );
+//   }}
